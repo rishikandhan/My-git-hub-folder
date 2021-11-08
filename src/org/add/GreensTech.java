ac18ee462a9b9e95bb75a6c413b4671de53b3673
rishikandhan <rishikandhan94@gmail.com>
2021-11-08 11:08:21 +0530

    first commit

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,46 @@
+package org.add;
+
+public class GreensTech {
+	
+	private void greensOmr() {
+
+		System.out.println("greens is omr : chennai");
+		
+		
+	}
+	
+	private void greensAdyar() {
+
+		System.out.println("greens is adyar : chennai");
+	}
+	
+	private void greensTambaram() {
+System.out.println("greens is tambaram: chennai");
+	}
+
+	private void greensvelacheery() {
+System.out.println("greens is velacheery : chennai");
+	}
+
+	private void greensAnnaNagar() {
+System.out.println("greens is annanagar : chennai");
+	}
+	public static void main(String[] args) {
+		 
+		GreensTech a = new GreensTech();
+		a.greensAdyar();
+		a.greensTambaram();
+		a.greensvelacheery();
+		a.greensAnnaNagar();
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
